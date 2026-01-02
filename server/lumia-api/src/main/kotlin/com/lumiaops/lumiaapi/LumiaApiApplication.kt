492c692963e980b2163abddd8ef61de47a574985
김명호 <kimmyungho1003@naver.com>
2026-01-02 14:54:53 +0900

    Fix: Add core module component scan to resolve lumia-api startup failure

--- server/lumia-api/src/main/kotlin/com/lumiaops/lumiaapi/LumiaApiApplication.kt
+++ server/lumia-api/src/main/kotlin/com/lumiaops/lumiaapi/LumiaApiApplication.kt
@@ -1,9 +1,15 @@
 package com.lumiaops.lumiaapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.lumiaops"])
+@EntityScan(basePackages = ["com.lumiaops"])
+@EnableJpaRepositories(basePackages = ["com.lumiaops"])
 class LumiaApiApplication
 
 fun main(args: Array<String>) {
